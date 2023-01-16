10cd0e09c25814733227394346571c54336b4b81
JavaNewAcc <java.new.acc@gmail.com>
2023-01-16 08:28:48 +0200

    updated HT9

--- js/3rdTask.js
+++ js/3rdTask.js
@@ -1,17 +1,19 @@
 window.onload = function () {
 
     let startPoint = document.querySelector(`.startPoint`);
-    let counterStart = 10;
+    let counterStart = 60;
     let counterDiv = document.createElement(`div`);
     counterDiv.setAttribute(`style`, `font-size:10rem`);
     counterDiv.setAttribute(`class`, `animatedCounter`);
     counterDiv.append(counterStart);
     startPoint.append(counterDiv);
+    let intervalID=""
 
     function counter() {
-        if (counterStart == 0) {
-            alert(`ВИ дочекались знижки!`);
-            clearInterval(counter);
+        if (counterStart <= 0) {
+            alert(`Ви дочекались знижки!`);
+            clearInterval(intervalID);
+            console.log(clearInterval(counter));
         }
         else {
             counterStart--;
@@ -21,6 +23,5 @@ window.onload = function () {
         }
     }
 
-    
-    // setInterval(counter, 1000);
+    intervalID = setInterval(counter, 1000);
 }
